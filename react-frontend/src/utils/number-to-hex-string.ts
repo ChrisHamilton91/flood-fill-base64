041fd041e4b0117a9714d5c318c62bdf6c015061
Chris Hamilton <chris_h_27@hotmail.ca>
2023-03-15 01:29:51 -0400

    Fix hex string formatting

--- react-frontend/src/utils/number-to-hex-string.ts
+++ react-frontend/src/utils/number-to-hex-string.ts
@@ -1,4 +1,4 @@
 /** Converts number (16777215) to hex string (#ffffff)  */
 export default function numberToHexString(color: number): string {
-  return "#" + color.toString(16);
+  return "#" + color.toString(16).padStart(6, "0");
 }
