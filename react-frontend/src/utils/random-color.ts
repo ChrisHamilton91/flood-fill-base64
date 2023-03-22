cacec38c4361f52e8477783ca9c6635b2300e972
Chris Hamilton <chris_h_27@hotmail.ca>
2023-03-22 03:05:33 -0400

    Fix hex string formatting

--- react-frontend/src/utils/random-color.ts
+++ react-frontend/src/utils/random-color.ts
@@ -1,4 +1,9 @@
 /** Generates random hex string from "#000000" to "#ffffff" */
 export default function randomColor(): string {
-  return "#" + Math.floor(Math.random() * 0x1000000).toString(16);
+  return (
+    "#" +
+    Math.floor(Math.random() * 0x1000000)
+      .toString(16)
+      .padStart(6, "0")
+  );
 }
